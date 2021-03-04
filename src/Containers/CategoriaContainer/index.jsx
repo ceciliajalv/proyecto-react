32fde9011cae97824c1f411174a87b73210ac49e
Cecilia Alvarez <alvarezceciliajacqueline@gmail.com>
2021-03-03 23:27:13 -0300

    Firebase y filtro de categoria

--- src/Containers/CategoriaContainer/index.jsx
+++ src/Containers/CategoriaContainer/index.jsx
@@ -4,16 +4,29 @@ import {useParams} from 'react-router-dom';
 import { ItemCount } from "../../components/ItemCount";
 import productList from "../../mocks/productList/index"
 import ItemList from "../../components/itemList/"
+import { getFirestore } from "../../firebase";
 
 const CategoriaContainer = () => {
     const {idCat} = useParams ();
     const [products, setProducts] = useState([]);
-    React.useEffect(()=>{
-        let categoria = productList.filter ( (element) => {
-            return element.categoria === idCat
-        });
-        setProducts(categoria);
-    }, [idCat]) 
+
+    const baseDeDatos = getFirestore();
+
+    const itemCollection = baseDeDatos.collection('item');
+    const catFilter = itemCollection.where('categoria', '==', idCat);
+
+    catFilter.get().then((querySnapshot) => {
+        if (querySnapshot.size === 0) {
+            console.log('No hubo resultados');
+        }
+        setProducts(querySnapshot.docs.map(doc => doc.data()));
+    })
+    // React.useEffect(()=>{
+    //     let categoria = productList.filter ( (element) => {
+    //         return element.categoria === idCat
+    //     });
+    //     setProducts(categoria);
+    // }, [idCat]) 
 
     return (
         <>
